1c7218400bc6401f3c2abdfd9c2fbb1572e4485a
carlmfe <cmfevang@gmail.com>
2020-09-20 20:56:35 +0200

    merging er vanskelig

--- Project2/code/JacobiEigSolver.hpp
+++ Project2/code/JacobiEigSolver.hpp
@@ -7,19 +7,22 @@ private:
 	double** A_;
 	int N_;
 	void ComputeSC_(int k, int l, double* pc, double* ps);
+	bool RUN;
+	double threshold_;
 	
 public:
 	JacobiEigSolver(double** A, int N);
 
-	void setA(double** A);
+	void setA(double** A, int N);
+	void CleanA(double threshold);
 	void getMax_(double* pmax, int* pk, int* pl);
 	void doJacobiRotation_(int k, int l);
 	double** Solve();
 	// void PrintEigenvalues();
 
 
-	void PrintMatrix();
+	void PrintMatrix(double** Matrix, int Dimension);
 	~JacobiEigSolver();
 };
 
-#endif 
+#endif
