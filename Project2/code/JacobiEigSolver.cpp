64dd3db64ce7cfc0a573aac3364bbe5fb83a157f
carlmfe <cmfevang@gmail.com>
2020-09-20 20:50:55 +0200

    Fikset 'smart'-versjonen av jacobirotasjonen.

--- Project2/code/JacobiEigSolver.cpp
+++ Project2/code/JacobiEigSolver.cpp
@@ -2,8 +2,11 @@
 #include <tuple>
 #include <cmath>
 #include "JacobiEigSolver.hpp"
+#include <iomanip>
+// #include <armadillo>
 
 using namespace std;
+// using namespace arma;
 
 JacobiEigSolver::JacobiEigSolver(double** A, int N) {
 	A_ = A;
@@ -14,18 +17,14 @@ JacobiEigSolver::JacobiEigSolver(double** A, int N) {
 void JacobiEigSolver::setA(double** A, int N) {
 	A_ = A;
 	N_ = N;
-	// for (int i=0; i<N; i++) {
-	// 	for (int j=0; j<N; j++) {
-	// 		A_[i][j] = A[i][j];
-	// 	}
-	// }
 }
 
 void JacobiEigSolver::CleanA(double threshold) {
 	for (int i=0; i<N_; i++) {
-		for (int j=i; j<N_; j++) {
+		for (int j=i+1; j<N_; j++) {
 			if (fabs(A_[i][j]) <= threshold) {
 				A_[i][j] = 0.0;
+				A_[j][i] = 0.0;
 			}
 		}
 	}
@@ -60,62 +59,26 @@ void JacobiEigSolver::ComputeSC_(int k, int l, double* pc, double* ps) {
 	*ps = t * *pc;
 }
 
-double** JacobiEigSolver::doJacobiRotation_(int k, int l) {
+void JacobiEigSolver::doJacobiRotation_(int k, int l) {
 	double s, c;
 	this->ComputeSC_(k, l, &c, &s);
 
-	// double akk = A_[k][k];
-	// double akl = A_[k][l];
-	// double all = A_[l][l];
-	// for (int i=0; i<N_; i++) {
-	// 	if (i!=k && i!=l) {
-	// 		double aik = A_[i][k];
-	// 		double ail = A_[i][l];
-	// 		double bik = A_[i][k]*c - A_[i][l]*s;
-	// 		double bil = A_[i][l]*c + A_[i][k]*s;
-	// 		A_[i][k] = bik;
-	// 		A_[k][i] = bik;
-	// 		A_[i][l] = bil;
-	// 		A_[l][i] = bil;
-	// 	}
-	// }
-	// A_[k][k] = akk*c*c - 2*akl*s*c + all*s*s;
-	// A_[l][l] = all*c*c + 2*akl*s*c + akk*s*s;
-	// A_[k][l] = 0.0;
-	// A_[l][k] = 0.0;
-
-	double** B = new double* [N_];
+	double akk = A_[k][k];
+	double all = A_[l][l];
+	A_[k][k] = akk*c*c - 2*A_[k][l]*s*c + all*s*s;
+	A_[l][l] = all*c*c + 2*A_[k][l]*s*c + akk*s*s;
+	A_[k][l] = 0.0;
+	A_[l][k] = 0.0;
 	for (int i=0; i<N_; i++) {
-		B[i] = new double [N_];
-	}
-
-	for (int i=0; i<N_; i++) {
-		for (int j=i; j<N_; j++) {
-			if (j==k && i!=k && i!=l) {
-				B[i][j] = A_[i][k] * c - A_[i][l] * s;
-				B[j][i] = A_[i][k] * c - A_[i][l] * s;
-			}
-			if (j==l && i!=k && i!=l) {
-				B[i][j] = A_[i][k] * c + A_[i][l] * s;
-				B[j][i] =  A_[i][k] * c + A_[i][l] * s;
-			}
-			if (i==k && j==k) {
-				B[i][j] = A_[k][k]*c*c - 2*A_[k][l]*c*s + A_[l][l]*s*s;
-			}
-			if (i==l && j==l) {
-				B[i][j] = A_[l][l]*c*c + 2*A_[k][l]*c*s + A_[k][k]*s*s;
-			}
-			if (i==k && j==l) {
-				B[i][j] = 0.0;
-				B[j][i] = 0.0;
-			}
-			else {
-				B[i][j] = A_[i][j];
-				B[j][i] = A_[j][i];
-			}
+		if (i != k && i != l) {
+			double aik = A_[i][k];
+			double ail = A_[i][l];
+			A_[i][k] = c*aik - s*ail;
+			A_[k][i] = A_[i][k];
+			A_[i][l] = c*ail + s*aik;
+			A_[l][i] = A_[i][l];
 		}
 	}
-	return B;
 }
 
 
@@ -124,28 +87,29 @@ double** JacobiEigSolver::Solve() {
 	double max;
 	int k, l;
 
-	double** B = new double* [N_];
-	for (int i=0; i<N_; i++) {
-		B[i] = new double [N_];
-	}
-
 	int iteration = 0;
+	this->CleanA(threshold_);
+	this->PrintMatrix(A_, N_);
+	max = 0.0;
+	this->getMax_(&max, &k, &l);
+
 	while (fabs(max)>threshold_ && iteration < 10) {
 		cout << "solving iteration " << iteration << endl;
-		max = 0.0;
-		this->getMax_(&max, &k, &l);
 		// cout << max << " " << k << " " << l << " " << endl;
-		B = this->doJacobiRotation_(k, l);
-		this->PrintMatrix(A_, N_);
-		this->setA(B, N_);
+		this->doJacobiRotation_(k, l);
+		// this->setA(B, N_);
 		this->CleanA(threshold_);
+		this->PrintMatrix(A_, N_);
 		iteration += 1;
 		// if (iteration > 10) {
 		// 	RUN = false;
 		// }
 		cout << endl;
-	}
+		// this->PrintEigenvalues()
+		max = 0.0;
+		this->getMax_(&max, &k, &l);
 
+	}
 	return A_;
 }
 
@@ -156,11 +120,20 @@ JacobiEigSolver::~JacobiEigSolver() {
 	}
 	delete [] A_;
 }
-
+/*
+void JacobiEigSolver::PrintEigenvalues() {
+	values = arga::eig_sym(A_);
+	cout << "Eigenvalues are: "
+	for (int=0; i<N_; i++){
+		cout << values[i] << ", "
+	}
+	cout << endl;
+}
+*/
 void JacobiEigSolver::PrintMatrix(double** Matrix, int Dimension) {
 	for(int i = 0; i < Dimension; i++) {
 		for(int j = 0; j < Dimension; j++) {
-			cout << Matrix[i][j] << " ";
+			cout << setprecision(3) << setw(10) << Matrix[i][j] << " ";
 		}
 		cout << endl;
 	}
--- Project2/code/JacobiEigSolver.hpp
+++ Project2/code/JacobiEigSolver.hpp
@@ -16,8 +16,9 @@ public:
 	void setA(double** A, int N);
 	void CleanA(double threshold);
 	void getMax_(double* pmax, int* pk, int* pl);
-	double** doJacobiRotation_(int k, int l);
+	void doJacobiRotation_(int k, int l);
 	double** Solve();
+	// void PrintEigenvalues();
 
 
 	void PrintMatrix(double** Matrix, int Dimension);
