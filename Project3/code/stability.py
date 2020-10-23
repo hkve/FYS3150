ea0fd2320a986934a5ce03054b395d216d2256bf
Håkon <haakon.kvernmoen@gmail.com>
2020-10-23 12:05:18 +0200

    Linfits and time

--- Project3/code/stability.py
+++ Project3/code/stability.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -9,9 +10,9 @@ from getInitialConditions import setInitialConditions
 from file_reader import read_data_file
 
 
-def check_init(filename, body_dict):	# Check if init file exists
+def check_init(filename, body_dict, fixedCoM):	# Check if init file exists
 	if not filename in os.listdir("initData"): 				
-		setInitialConditions(filename, body_dict)
+		setInitialConditions(filename, body_dict, fixedCoM)
 	else:
 		pass
 
@@ -92,12 +93,9 @@ def plot_circular_orbit(dt=0.0001, T_end=1, method="verlet", N_write=1000):
 		plt.show()
 	
 
-def plot_error(N_start=1, N_end=9, n_tests=10):
-	log_start, log_end = np.log10(N_start), np.log10(N_end)
-	N = np.logspace(log_start, log_end, n_tests, endpoint=True)
-	N = (10**N).astype(int)
+def plot_error(dt_start=-1, dt_end=-8, n_tests=30):
+	dt = np.logspace(dt_start, dt_end, n_tests, endpoint=True)
 	
-
 	methods = ["euler", "verlet"]
 
 	initFilename = "SunEarthStable_init.dat"
@@ -105,22 +103,21 @@ def plot_error(N_start=1, N_end=9, n_tests=10):
 	outFilenames = []
 	for method in methods:
 		for i in range(n_tests):
-			outFilenames.append(f"SunEarthStable_{method}_{N_start}_{N_end}_{i+1}.dat") 
+			outFilenames.append(f"SunEarthStable_{method}_{dt_start}_{dt_end}_{i+1}.dat") 
 
 	body_dict = {"Sun": [0,0,0,0,0,0],
 				 "Earth": [1,0,0,0,2*np.pi,0]}
 	
-	setInitialConditions(initFilename, body_dict)
+	check_init(initFilename, body_dict, fixedCoM=True)
 
-	#check_init(initFilename, body_dict)
 	exists = has_data(outFilenames)
 	i = 0
 
 	if not exists:
 		for method in methods:
-			for n in N:
-				dt = 1/n
-				master_call = f"python3 master.py {dt} {n} -method {method} -sys initData/{initFilename} -out {outFilenames[i]} -Nwrite {2} -time years --q" 
+			for dt_ in dt:
+				n_ = int(1/dt_)
+				master_call = f"python3 master.py {dt_} {n_} -method {method} -sys initData/{initFilename} -out {outFilenames[i]} -Nwrite {2} -time years --q" 
 				subprocess.call(master_call.split())
 				i += 1
 
@@ -129,30 +126,48 @@ def plot_error(N_start=1, N_end=9, n_tests=10):
 	verletError = []
 
 	for outfile in outFilenames:
-		try:
-			system = read_data_file(outfile)
-			
-			r = system["Earth"].r
-			#print(r, "r", r[0,-1], r[1,-1])
-			error = np.abs(r[0,0] - r[0,-1])#np.abs(1-np.sqrt((r[0,-1])**2 + r[1,-1]**2))
-			print(error)
-			
+		system = read_data_file(outfile)
+		
+		rS = system["Sun"].r
+		rE = system["Earth"].r
 
-			if system["method"] == 0:
-				# print("euler error: ", rx[0], rx[-1])
-				eulerError.append(error)
-			if system["method"] == 1:
-				# print("verlet error: ", rx[0], rx[-1])
-				verletError.append(error)
-		except:
-			verletError.append(1)
+		r0 = np.linalg.norm(rE[:,0]+rS[:,0])
+		rN = np.linalg.norm(rE[:,-1]+rS[:,-1])
+		error = np.abs((r0-rN)/r0)
+
+		if system["method"] == 0:
+			eulerError.append(error)
+		if system["method"] == 1:
+			verletError.append(error)
 	
 	with sns.axes_style("darkgrid"):
 		fix, ax = plt.subplots()
-		ax.set(xscale="log", yscale="log", xlabel="N", ylabel="Relative error")
+		ax.set(xscale="log", yscale="log", xlabel="dt", ylabel="Relative error")
+		ax.invert_xaxis()
+		ax.scatter(dt, eulerError, c="blue", label="Euler")
+
+		ax.scatter(dt, verletError, c="orange", label="Verlet")
 		
-		ax.scatter(N, eulerError, label="Euler")
-		ax.scatter(N, verletError, label="Verlet")
+		# Only do linfit if standard params (as some points have to be excluded)
+		if all([val is plot_error.__defaults__[i] for i, val in enumerate([dt_start,dt_end,n_tests])]): 
+			sE, eE = 3, len(dt) # Euler points to linfit (start, end)
+			sV, eV = 0, 11      # Verlet points to linfit (start, end)
+			
+			# Linfit for Euler
+			slopeE, constE, r_valueE, p_valueE, std_errE = \
+			stats.linregress(np.log10(dt[sE:eE]), np.log10(eulerError[sE:eE]))
+
+			# Linfit for Verlet
+			slopeV, constV, r_valueV, p_valueV, std_errV = \
+			stats.linregress(np.log10(dt[sV:eV]), np.log10(verletError[sV:eV]))
+
+			# Plot Euler and Verlet linfit
+			ax.plot(dt[sE:eE], 10**constE * dt[sE:eE]**slopeE, c="red",\
+					label=f"Slope Euler = {slopeE:.3f}$\pm${std_errE:.3f}", markersize=3)
+			ax.plot(dt[sV:eV], 10**constV * dt[sV:eV]**slopeV, c="k" ,\
+					label=f"Slope Verlet = {slopeV:.3f}$\pm${std_errV:.3f}", markersize=3)
+		
+
 		ax.legend()
 		plt.show()
 	
@@ -166,7 +181,7 @@ def plot_energy(N=int(1e7), T_end = 50, N_write=10000):
 	body_dict = {"Sun": [0,0,0,0,0,0],
 				 "Earth": [1,0,0,0,2*np.pi,0]}
 
-	check_init(initFilename, body_dict)
+	check_init(initFilename, body_dict, fixedCoM=True)
 
 	outFilenames = []
 	for method in methods:
@@ -200,4 +215,72 @@ def plot_energy(N=int(1e7), T_end = 50, N_write=10000):
 	ax.legend()
 	plt.show()
 
-plot_error()
+def plot_time(N_start=2, N_end=8, n_tests=30):
+	N = np.logspace(N_start, N_end, n_tests, endpoint=True, dtype=int)
+	
+	methods = ["euler", "verlet"]
+
+	initFilename = "SunEarthStable_init.dat"
+
+	outFilenames = []
+	for method in methods:
+		for i in range(n_tests):
+			outFilenames.append(f"SunEarthStable_{method}_{N_start}_{N_end}_{i+1}.dat") 
+
+	body_dict = {"Sun": [0,0,0,0,0,0],
+				 "Earth": [1,0,0,0,2*np.pi,0]}
+	
+	check_init(initFilename, body_dict, fixedCoM=True)
+
+	exists = has_data(outFilenames)
+	i = 0
+
+	if not exists:
+		for method in methods:
+			for n_ in N:
+				dt_ = 1/n_
+				master_call = f"python3 master.py {dt_} {n_} -method {method} -sys initData/{initFilename} -out {outFilenames[i]} -Nwrite {2} -time years --q" 
+				subprocess.call(master_call.split())
+				i += 1
+
+	eulerTime = []
+	verletTime = []
+	for outfile in outFilenames:
+		system = read_data_file(outfile)
+		
+		if system["method"] == 0:
+			eulerTime.append(system["time"])
+		if system["method"] == 1:
+			verletTime.append(system["time"])
+
+
+	with sns.axes_style("darkgrid"):
+		fig, ax = plt.subplots()
+		ax.set(xscale="log", yscale="log")
+		ax.set_xlabel(xlabel="N", fontsize=13)
+		ax.set_ylabel(ylabel="Time [s]", fontsize=13)
+		ax.scatter(N, eulerTime, label="Euler")
+		ax.scatter(N, verletTime, label="Verlet")
+	
+		# Only linfit if standard params (as some points have to be excluded)
+		if all([val is plot_time.__defaults__[i] for i, val in enumerate([N_start,N_end,n_tests])]):
+			sE, eE = 15, len(N)
+			sV, eV = 15, len(N)
+
+			slopeE, constE, r_valueE, p_valueE, std_errE = \
+			stats.linregress(np.log10(N[sE:eE]), np.log10(eulerTime[sE:eE]))
+
+			# Linfit for Verlet
+			slopeV, constV, r_valueV, p_valueV, std_errV = \
+			stats.linregress(np.log10(N[sV:eV]), np.log10(verletTime[sV:eV]))
+
+			# Plot Euler and Verlet linfit
+			ax.plot(N[sE:eE], 10**constE * N[sE:eE]**slopeE, c="red",\
+					label=f"Slope Euler = {slopeE:.3f}$\pm${std_errE:.3f}", markersize=3)
+			ax.plot(N[sV:eV], 10**constV * N[sV:eV]**slopeV, c="k" ,\
+					label=f"Slope Verlet = {slopeV:.3f}$\pm${std_errV:.3f}", markersize=3)
+		
+	ax.legend(fontsize=13)
+	plt.show()
+
+plot_time()
