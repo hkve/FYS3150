2e5774c2e62066d4a51390d62d18fce93664f9eb
Bror Hjemgaard <bahjemga@uio.no>
2020-10-30 20:13:21 +0100

    Small tweaks

--- Project3/code/getInitialConditions.py
+++ Project3/code/getInitialConditions.py
@@ -46,7 +46,7 @@ def grabBody(UUID, date=None):
 	
 	return init
 
-def getInitialCondition(filename, bodies=None, date=None, fixedCoM=False, scaled_mass=None):
+def getInitialCondition(filename, bodies=None, date=None, fixedCoM=False, scaled_mass=None, timeFormat="days"):
 	"""
 	Takes a list of bodies and writes initial condtions to filename
 
@@ -54,6 +54,7 @@ def getInitialCondition(filename, bodies=None, date=None, fixedCoM=False, scaled
 		filename: String, name for file to write to
 		bodies:   String list, name of bodies for initial conditions. If None all bodies are written
 		date: 	  String, YYYY-MM-DD, Date for intial conditions, default is todays date
+		timeFormat:   String, "days" or "years". velocities will be measured in AU/days or AU/years
 
 	"""
 	
@@ -76,6 +77,11 @@ def getInitialCondition(filename, bodies=None, date=None, fixedCoM=False, scaled
 			print(f"{body} not available, skipping")
 			continue
 		body_dict[body] = grabBody(UUIDs[body]).split(",")[1:]
+	if timeFormat == "years":
+		for name, val in body_dict.items():
+			new = val[:3] + [str(float(foo)*365.25) for foo in val[3:]]
+			body_dict[name] = new
+
 	setInitialConditions(filename, body_dict, fixedCoM, scaled_mass)
 	print(f"Saved to {filename}, wrote {len(body_dict)} bodies")
 
@@ -94,9 +100,9 @@ def setInitialConditions(filename, body_dict, fixedCoM = False, scaled_mass=None
 	UUIDs, Masses  = getUUIDs(DIR2SAVE+"/bodyUUID.txt")
 
 	# If some masses should be scaled
-	if scaled_mass != None:
-		for body in scaled_mass.keys():
-			Masses[body] *= scaled_mass[body]
+	# if scaled_mass != None:
+	# 	for body in scaled_mass.keys():
+	# 		Masses[body] *= scaled_mass[body]
 
 	# Checking if body is available and making x and v strings
 	for body in body_dict:
