62286af7578209fadee1e141480870589dea6ca1
Bror Hjemgaard <bahjemga@uio.no>
2020-11-09 10:44:04 +0100

    made animation conotonoius

--- Project4/code/python/latticeAnimation.py
+++ Project4/code/python/latticeAnimation.py
@@ -7,11 +7,11 @@ data = np.loadtxt("../data/lattice.out")
 fix, ax = plt.subplots()
 L = 300 
 
-
-for i in range(data.shape[0]):
-	data_ = data[i].reshape(L,L)
-	ax.cla()
-	ax.imshow(data_, interpolation="nearest")
-	plt.pause(0.5)
+while True:
+	for i in range(data.shape[0]):
+		data_ = data[i].reshape(L,L)
+		ax.cla()
+		ax.imshow(data_, interpolation="nearest")
+		plt.pause(0.5)
 plt.show()
 
