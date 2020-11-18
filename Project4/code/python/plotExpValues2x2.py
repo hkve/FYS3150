97e55f8025194dedb49a1776bbf94c07a776e6ca
brorh <bror.hjemgaard@gmail.com>
2020-11-18 19:10:02 +0100

    update plotting code

--- Project4/code/python/plotExpValues2x2.py
+++ Project4/code/python/plotExpValues2x2.py
@@ -4,7 +4,7 @@ def main(sim = False):
 	import seaborn as sns
 	from subprocess import run
 	
-	Tstart = 2
+	Tstart = 1
 	Tend = 4
 	dt = 0.1
 	MCCs = int(1e6)
@@ -15,7 +15,7 @@ def main(sim = False):
 		run(f"../cpp/2x2_lattice.out {Tstart} {Tend} {dt} {MCCs} {filename}".split())
 			
 	data = np.loadtxt("../data/2x2EXP.out")
-	print(data)
+	L = 2
 	E, M, E2, M2, Mabs, varE, varM, T = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5], data[:,6], data[:,7]
 	CV = varE/T**2
 	X = varM/T
@@ -26,24 +26,30 @@ def main(sim = False):
 	X_ana = 32/(T*Z) * (1+np.exp(8/T))
 
 	with sns.axes_style("darkgrid"):
-		fig, axes = plt.subplots(nrows=2,ncols=2)
-		
-		axes[0,0].scatter(T, E)
-		axes[0,1].scatter(T, Mabs)
-		axes[1,0].scatter(T, CV)
-		axes[1,1].scatter(T, X)
+		fig, axes = plt.subplots(nrows=2,ncols=2, dpi=100)
+		markersize =120
+		markeralpha = 0.9
+		anastyle = "solid"
+		anasize = 4
+		anaalpha = 1
+		axes[0,0].scatter(T, E/L**2,label="Data", marker=".", s=markersize, c="k", alpha=markeralpha,zorder=100)
+		axes[0,1].scatter(T, Mabs/L**2, marker=".", s=markersize, c="k", alpha=markeralpha,zorder=100)
+		axes[1,0].scatter(T, CV/L**2, marker=".",s=markersize, c="k", alpha=markeralpha,zorder=100)
+		axes[1,1].scatter(T, X/L**2,marker=".", s=markersize, c="k", alpha=markeralpha,zorder=100)
 
-		axes[0,0].plot(T, E_ana, c="r")
-		axes[0,1].plot(T, Mabs_ana, c="r")
-		axes[1,0].plot(T, Cv_ana, c="r")
-		axes[1,1].plot(T, X_ana, c="r")
+		axes[0,0].plot(T, E_ana/L**2,label="Analytical", c="r", lw=anasize, alpha=anaalpha)
+		axes[0,1].plot(T, Mabs_ana/L**2, c="r", lw=anasize, alpha=anaalpha)
+		axes[1,0].plot(T, Cv_ana/L**2, c="r", lw=anasize, alpha=anaalpha)
+		axes[1,1].plot(T, X_ana/L**2, c="r", lw=anasize, alpha=anaalpha)
 
 		fontsize=14
 		axes[0,0].set_ylabel(r"$\langle E \rangle$", fontsize=fontsize)
-		axes[0,1].set_ylabel(r"$\langle M \rangle$", fontsize=fontsize)
+		axes[0,1].set_ylabel(r"$\langle |M| \rangle$", fontsize=fontsize)
 		axes[1,0].set_ylabel("$C_v$", fontsize=fontsize)
 		axes[1,1].set_ylabel("$\chi$", fontsize=fontsize)
-		axes[1,0].set_xlabel("T", fontsize=fontsize)
-		axes[1,1].set_xlabel("T", fontsize=13)
+		axes[1,0].set_xlabel("$T$  $[ kT$ / $J]$", fontsize=fontsize)
+		axes[1,1].set_xlabel("$T$  $[ kT$ / $J]$", fontsize=13)
+	lines, labels = axes[0,0].get_legend_handles_labels()
+	fig.legend(lines,labels,loc='upper center', ncol=2, fancybox=True, shadow=True,fontsize=14)
 	plt.show()
 
--- Project4/code/python/plotMCS2x2.py
+++ Project4/code/python/plotMCS2x2.py
@@ -5,7 +5,7 @@ def main(sim = False):
 	import seaborn as sns
 	import sys
 
-	T_ = [1,3]
+	T_ = [1]
 	N = 200
 	MCCs = np.logspace(1, 6, N, dtype=int, endpoint=True)
 	
@@ -20,32 +20,64 @@ def main(sim = False):
 
 
 	with sns.axes_style("darkgrid"):
-		fig, axes = plt.subplots(nrows=1, ncols=2)
-		axes[0].set(xscale="log")
-		axes[1].set(xscale="log")
+		fig, axes = plt.subplots(nrows=2, ncols=2, dpi=100)
+		axes[0,0].set(xscale="log")
+		axes[0,1].set(xscale="log")
+		axes[1,0].set(xscale="log")
+		axes[1,1].set(xscale="log")
 		for filename in filenames:
 			data = np.loadtxt(f"../data/{filename}")
-
-			T = data[0,-2]
-			MCCs = data[:,-1]
-			E, Mabs = data[:,0], data[:,4]
-			
+			E, M, E2, M2, Mabs, varE, varM, T, MCCs = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5], data[:,6], data[:,7], data[:,8]
+			L = 2
+			#T = data[0,-2]	
+			#MCCs = data[:,-1]
+			#E, Mabs = data[:,0], data[:,4]
+			E = E/L**2
+			Mabs = Mabs/L**2
 			Z = 12 + 4*np.cosh(8/T)
-			E_ana = -32/Z * np.sinh(8/T) 
-			Mabs_ana = 8/Z * (2+np.exp(8/T))
+			CV = varE/T**2/L**2
+			X = varM/T/L**2
+			Cv_ana = 1024/(T*Z)**2 *(3*np.cosh(8/T)+1)/L**2
+			X_ana = 32/(T*Z) * (1+np.exp(8/T))/L**2
+			E_ana = -32/Z * np.sinh(8/T) /L**2
+			Mabs_ana = 8/Z * (2+np.exp(8/T))/L**2
 
-			E_rel_error = np.abs((E-E_ana)/E_ana)
-			Mabs_rel_error = np.abs((Mabs-Mabs_ana)/Mabs_ana)
+			#E_rel_error = np.abs((E-E_ana)/E_ana)
+			#Mabs_rel_error = np.abs((Mabs-Mabs_ana)/Mabs_ana)
 			
 			#E = E/np.linalg.norm(E)
 			#Mabs = Mabs/np.max(Mabs)
-			
-			axes[0].scatter(MCCs, E, label=f"{T}")
-			axes[0].plot([MCCs[0],MCCs[-1]], [E_ana, E_ana], c="k", linestyle="dashed")
-			axes[1].scatter(MCCs, Mabs, label=f"{T}")
-			axes[1].plot([MCCs[0],MCCs[-1]], [Mabs_ana, Mabs_ana], c="k", linestyle="dashed")
+			markersize = 50
+			markeralpha = 0.6
+			anastyle = "solid"
+			anasize = 3
+			anaalpha = 0.6
+			axes[0,0].scatter((),(), label="Data", marker="+", c="k",s=80)
+
+			axes[0,0].scatter(MCCs, E,  marker="+", c="k",s=markersize, alpha=markeralpha)
+			axes[0,0].plot([MCCs[0],MCCs[-1]], [E_ana[0], E_ana[-1]], c="r", linestyle=anastyle, lw = anasize, label=f"Analytical", alpha=anaalpha)
+			axes[0,1].scatter(MCCs, Mabs, marker="+", c="k",s=markersize, alpha=markeralpha)
+			axes[0,1].plot([MCCs[0],MCCs[-1]], [Mabs_ana[0], Mabs_ana[-1]], c="r", linestyle=anastyle, lw = anasize, alpha=anaalpha)
+			axes[1,0].scatter(MCCs, CV, marker="+", c="k",s=markersize, alpha=markeralpha)
+			axes[1,0].plot([MCCs[0], MCCs[-1]], [Cv_ana[0],Cv_ana[-1]], c="r",linestyle=anastyle, lw = anasize, alpha=anaalpha)
+			axes[1,1].scatter(MCCs, X, marker="+", c="k", s=markersize, alpha=markeralpha)
+			axes[1,1].plot([MCCs[0], MCCs[-1]], [X_ana[0],X_ana[-1]], c="r",linestyle=anastyle, lw = anasize, alpha=anaalpha)
+			fontsize=14
+			axes[0,0].set_ylabel(r"$\langle E \rangle$", fontsize=fontsize)
+			axes[0,1].set_ylabel(r"$\langle | M| \rangle$", fontsize=fontsize)
+			axes[1,0].set_ylabel("$C_v$", fontsize=fontsize)
+			axes[1,1].set_ylabel("$\chi$", fontsize=fontsize)
+			axes[1,0].set_xlabel("MCCs", fontsize=fontsize)
+			axes[1,1].set_xlabel("MCCs", fontsize=fontsize)
 		
-		lines, labels = fig.axes[0].get_legend_handles_labels()
-		fig.legend(lines,labels,loc='upper center', ncol=2, fancybox=True, shadow=True,fontsize=13)
+		axes[0,0].set_xticks([10**1, 10**2, 10**3, 10**4,10**5,10**6])
+		axes[0,1].set_xticks([10**1, 10**2, 10**3, 10**4,10**5,10**6])
+		axes[1,0].set_xticks([10**1, 10**2, 10**3, 10**4,10**5,10**6])
+		axes[1,1].set_xticks([10**1, 10**2, 10**3, 10**4,10**5,10**6])
+
+		axes[0,1].set_ylim([0.85,1.01])
+		axes[0,1].set_yticks([0.85, 0.9, 0.95, 1.0])
+		lines, labels = axes[0,0].get_legend_handles_labels()
+		fig.legend(lines,labels,loc='upper center', ncol=2, fancybox=True, shadow=True,fontsize=14)
 		plt.show()
 	
