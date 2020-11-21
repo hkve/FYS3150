16734aa608271af8820f23aad8ce3d3584695cfa
brorh <bror.hjemgaard@gmail.com>
2020-11-21 15:20:51 +0100

    Phase

--- Project4/code/python/plotPhaseTransition.py
+++ Project4/code/python/plotPhaseTransition.py
@@ -1,24 +1,36 @@
-def main(sim = False):
+def getkwarg(key, kwargs):
+    try:
+        if key in kwargs.keys():
+            return kwargs[key]
+    except:
+        return None
+
+def main(sim = False, **kwargs):
     import numpy as np
     import matplotlib.pyplot as plt
     from subprocess import run
     import seaborn as sns
     import sys
-    L_ = np.array([40,60,80,100])
+    import scipy.interpolate as pol
+
+    L_ = np.array([2,40])#40,60])#,80,100])
+    L_ = [100]
     #L_ = np.array([100])
     dT = 0.01
-    Tstart = 2
-    Tend = 2.3
+    Tstart = 1.5
+    Tend = 3.5
+    stable_logMCCs = 4.3
+    stable_MCCs = int(10**stable_logMCCs)
     logMCCs = 5
     MCCs = int(10**logMCCs)
 
-    filenames = [f"paralell_L{L}_T0{Tstart}_T1{Tend}_dT{dT}_MCCs{logMCCs}.dat" for L in L_]
-
+    filenames = [f"paralell_L{L}_T0{Tstart}_T1{Tend}_dT{dT}_MCCs{stable_logMCCs}.dat" for L in L_]
+    print(filenames)
     if sim:
         for L,filename in zip(L_,filenames):
             #run(f"rm ../data/{filename}".split())
             print(L)
-            #run(f"../cpp/paralell.out {L} {MCCs} {Tstart} {Tend+dT} {dT} {filename}".split())
+            #run(f"../cpp/paralell.out {L} {MCCs} {stable_MCCs} {Tstart} {Tend+dT} {dT} {filename}".split())
 
 
     with sns.axes_style("darkgrid"):
@@ -29,8 +41,9 @@ def main(sim = False):
             E, M, E2, M2, Mabs, varE, varM, T, MCCs = data[:,0], data[:,1], data[:,2], data[:,3], data[:,4], data[:,5], data[:,6], data[:,7], data[:,8]
             sortIdx = np.argsort(T)
 
-            T = np.array([T[idx] for idx in sortIdx])
+            T = np.array([T[idx] for idx in sortIdx])-dT
             E = np.array([E[idx] for idx in sortIdx])/L**2
+            M = np.array([M[idx] for idx in sortIdx])/L**2
             Mabs = np.array([Mabs[idx] for idx in sortIdx])/L**2
             varE = np.array([varE[idx] for idx in sortIdx])/L**2
             varM = np.array([varM[idx] for idx in sortIdx])/L**2
@@ -38,7 +51,22 @@ def main(sim = False):
             X = varM/T
 
             for ax, param in zip(np.array(axes).flatten(), [E,Mabs, CV, X]):
-                ax.plot(T[:],param[:], "-o", color=colors[i], alpha=0.7,label=f"$L={L}$")
+
+                pops = 28
+                pops = [1.5,1.540,1.620,1.63,1.64,1.66,1.68,1.74,1.76,1.83,1.94]
+                T_ = list(T)[:]
+                param_ = list(param)[:]
+                for val in pops:
+                    midx = T_.index(val-dT)
+                    T_.pop(midx)
+                    param_.pop(midx)
+
+
+                cs = pol.CubicSpline(T_,param_)
+                Ts = np.linspace(T_[0],T_[-1],1000)
+                ax.plot(Ts,cs(Ts) , color=colors[i], alpha=0.7,label=f"$L={L}$")
+                # ax.plot(T_, param_ , "-o", color=colors[i], alpha=0.7,label=f"$L={L}$")
+
             print(colors[i], L)
             
 
