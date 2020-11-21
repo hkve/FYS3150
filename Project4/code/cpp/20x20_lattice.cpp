a15856ddff4ba0f963c5db89b754f422551e7011
Håkon <haakon.kvernmoen@gmail.com>
2020-11-21 15:17:04 +0100

    20x20_lattice.cpp now only needs T, MCCs (total), initSpin and filename. Run once for one temp and one init spin

--- Project4/code/cpp/20x20_lattice.cpp
+++ Project4/code/cpp/20x20_lattice.cpp
@@ -6,25 +6,18 @@ int main(int argc, char const *argv[])
     
 	/*args:
     T (double): Temperature in kT/J
-    MCCs_start (double): Cycle start 
-    MCCs_end (double): Cycle end
-    dMCCs (int): Number of MCCs between each MCCs calculated between start and end
+    MCCs (int): Number of cycles to run 
     initSpin (int): 0 or 1. The orientation of the init spins. 0 for random, 1 for all positively aligned
+    filename (string) Name of file to save in data folder
     */
     
 	double T = atof(argv[1]);
     int MCCs = atof(argv[2]);
     int initSpin = atoi(argv[3]);
-	string filename = (string) argv[4];//"../data/20x20Values.out";
-	//string filename = "../data/20x20Values.out";
+	string filename = (string) argv[4];
     IsingModel* problem = new IsingModel(20,MCCs,1,T);
     problem->Initialize(initSpin);
-    problem->Solve();
-    problem->writeFinalExpValues(filename);
-    // int MCCs_start = atof(argv[2]);
-    // int MCCs_end = atof(argv[3]);
-    // int dMCCs = atoi(argv[4]);
-    //string outfile = (string)argv[6];
+    problem->Solve_ExpValuesMeanwhile(filename); // This saves ExpValues at every cycle
 
 
 	
