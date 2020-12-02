54ecd28dcb00c4075103c97df798b0a491c371a8
brorh <bror.hjemgaard@gmail.com>
2020-12-02 18:07:02 +0100

    updated alpha solver

--- Project5/code/VMC/variateAlpha.cpp
+++ Project5/code/VMC/variateAlpha.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
 		cerr << "Bad usage, enter log10(MCCs), alphaStart, alphaEnd, dAlpha" <<endl;
 		exit(1);
 	}
-	else if(argc == 6) {
+	else if(argc == 7) {
 		mode = argv[5];
 		if(mode != "interactive" && mode != "noninteractive") {
 			cerr << mode << " is not a valid mode, try 'interactive' or 'noninteractive'" <<endl;
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]) {
 	double alphaStart = atof(argv[2]);
 	double alphaEnd = atof(argv[3]);
 	double dAlpha = atof(argv[4]);
+	string solveMode = (string)argv[6];
 	
 	string filenameMode = mode;
 	filenameMode[0] = toupper(filenameMode[0]);
@@ -47,8 +48,17 @@ int main(int argc, char *argv[]) {
 	for(int i = 0; i < N_alpha; i++) {
 		alpha = alphaStart + i*dAlpha;
 		VMC* problem = new VMC(psi, EL, MCCs, step, omega, alpha);
-		problem->Run(filename + to_string(i) + ".dat", "log", 1000);
+		if (solveMode == "NoSave"){
+			problem->Run_NoSave();// filename + to_string(i) + ".dat", "log", 1000);
+			//problem->WriteFinal(filename + to_string(i) + ".dat");
+			problem->WriteFinal("alpha" + mode + ".dat");
+		}
+		else {
+			problem->Run(filename + to_string(i) + ".dat", "log", 1000);
+		}
 		cout << "Done alpha = " << alpha << " " << mode << endl; 
+
+		delete problem;
 	}
 	
 }
