f9b4fc026322f0265f326ff44cda86ddca45393a
brorh <bror.hjemgaard@gmail.com>
2020-12-03 21:02:15 +0100

    plotting files. will rename later

--- Project5/code/plotting/plotVarAlpha.py
+++ Project5/code/plotting/plotVarAlpha.py
@@ -1,36 +1,54 @@
+import matplotlib as mpl
+from colour import Color
+
 def varAlpha(sim = False): 
 	import numpy as np
 	import matplotlib.pyplot as plt
 	import os
+	import seaborn as sns
 
 	MCCs = 7
 	alphaStart = 0.5
 	alphaEnd = 1.5
-	dAlpha = 0.1
+	dAlpha = 0.05
 
 	N_alpha = round(((alphaEnd-alphaStart)/dAlpha),0)
 	N_alpha = int(N_alpha) + 1
+	
 
 	alphas = np.linspace(alphaStart, alphaEnd, N_alpha, endpoint=True)
+	colors = list(Color("pink", luminance=0.5).range_to(Color("green", luminance=0.5),N_alpha)) 
+	colors = [color.get_rgb() for color in colors]
+	for filename, mode,title in zip(["varAlphaNoninteractive", "varAlphaInteractive"], ["noninteractive", "interactive"], ["Without interactions", "With interactions"]):
+		if sim:
+			os.system(f"../compiled/variateAlpha.exe {7} {alphaStart} {alphaEnd} {dAlpha} {mode} foo")
+		with sns.axes_style("darkgrid"):
+			fig, axes = plt.subplots(nrows=1, ncols=2, dpi=120)
+			axes[0].set(xscale="log")
+			axes[1].set(xscale="log")
+			for i, alpha in enumerate(alphas):
+				data = np.loadtxt(f"../data/{filename}{i}.dat")
+				cyles, E, E2 = data[:,0], data[:,1], data[:,2]
 
-	if sim:
-		os.system(f"../compiled/variateAlpha.exe {7} {alphaStart} {alphaEnd} {dAlpha} noninteractive")
+				varE = E2-E*E
+				zorder = 1#int(10-10*np.abs(alpha -1))
+				axes[0].plot(cyles, E, color=colors[i], zorder=zorder, lw=1.7,alpha=0.72)#, label=fr"$\alpha$ = {alpha:.1f}, E = {E[-1]:.2f}")
+				axes[1].plot(cyles, varE, color=colors[i], zorder=zorder,lw=1.7,alpha=0.72)#, label=fr"$\alpha$ = {alpha:.1f} $\sigma_E^2$ = {varE[-1]:.2f}")
+			cmap = mpl.colors.ListedColormap(colors)
+			norm = mpl.colors.Normalize(vmin=alphas[0],vmax=alphas[-1])
+			cbar = axes[1].figure.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=axes[1], orientation='vertical',ticks=[1.5, 1.25, 1, 0.75, 0.5] )
 
-	fig, axes = plt.subplots(nrows=1, ncols=2)
-	axes[0].set(xscale="log")
-	axes[1].set(xscale="log")
-	for i, alpha in enumerate(alphas):
-		data = np.loadtxt(f"../data/varAlphaNoninteractive{i}.dat")
-		cyles, E, E2 = data[:,0], data[:,1], data[:,2]
+			cbar.ax.set_ylabel(r'  $\alpha$', rotation=0, fontsize=14)
+			axes[0].set_title(f"Energy, {title}")
+			axes[1].set_title(f"Variance of energy, {title}")
 
-		varE = E2-E*E
-		skip = 0
-		axes[0].plot(cyles[skip:], E[skip:], label=fr"$\alpha$ = {alpha:.1f}, E = {E[-1]:.2f}")
-		axes[1].plot(cyles[skip:], varE[skip:], label=fr"$\alpha$ = {alpha:.1f} $\sigma_E^2$ = {varE[-1]:.2f}")
+			axes[0].set_ylabel(r"$E_{L1}$ $[\omega]$", fontsize=15)
+			axes[0].set_xlabel(r"$\tau$", fontsize=15)
+			axes[1].set_ylabel(r"$\sigma_{E_{L1}}^2$  $[\omega^2]$", fontsize=14)
+			axes[1].set_xlabel(r"$\tau$", fontsize=15)
+			plt.show()
+			
 
-	axes[0].legend()
-	axes[1].legend()
-	plt.show()
 
 def varAlphaInteractive(sim = False):
 	import numpy as np
@@ -48,7 +66,7 @@ def varAlphaInteractive(sim = False):
 	alphas = np.linspace(alphaStart, alphaEnd, N_alpha, endpoint=True)
 
 	if sim:
-		os.system(f"../compiled/variateAlpha.exe {7} {alphaStart} {alphaEnd} {dAlpha} interactive")
+		os.system(f"../compiled/variateAlpha.exe {5} {alphaStart} {alphaEnd} {dAlpha} interactive a")
 
 	fig, axes = plt.subplots(nrows=1, ncols=2)
 	axes[0].set(xscale="log")
@@ -66,3 +84,5 @@ def varAlphaInteractive(sim = False):
 	axes[1].legend()
 	plt.show()
 
+varAlpha(sim=False)
+#varAlphaInteractive(sim = True)
--- Project5/code/plotting/plotVarAlpha2.py
+++ Project5/code/plotting/plotVarAlpha2.py
@@ -2,6 +2,7 @@ def varAlpha(sim = False):
 	import numpy as np
 	import matplotlib.pyplot as plt
 	import os
+	import seaborn as sns
 
 	#MCCs = 7
 	alphaStart = 0.5
@@ -10,78 +11,34 @@ def varAlpha(sim = False):
 
 	N_alpha = round(((alphaEnd-alphaStart)/dAlpha),0)
 	N_alpha = int(N_alpha) + 1
-
-	fig, axes = plt.subplots(nrows=1, ncols=2)
 	alphas = np.linspace(alphaStart, alphaEnd, N_alpha, endpoint=True)
-	E_ = []
+	colors = ["blue","red"][::-1]
 	r12_ = []
-	for mode in ["interactive", "noninteractive"]:
-		if sim:
-			os.system(f"../compiled/variateAlpha.exe {5} {alphaStart} {alphaEnd} {dAlpha} {mode} NoSave")
+	labels = ["With interactions", "Without interactions"]
+	linestyles = ["solid", "dashed"]
+	with sns.axes_style("darkgrid"):
+		fig, axes = plt.subplots(nrows=1, ncols=2, dpi=120)
+		for i,mode in enumerate(["interactive", "noninteractive"]):
+			if sim and i != 0:
+				os.system(f"../compiled/variateAlpha.exe {7} {alphaStart} {alphaEnd} {dAlpha} {mode} NoSave")
 
-	#axes.set(xscale="log")
-	#axes[1].set(xscale="log")
-	
-		cycles, beta, E, E2, r12 = np.loadtxt(f"../data/alpha{mode}.dat", unpack=True, usecols=(0,2,3,4,5))
-		#	file << MCCs << " " << alpha << " " << beta << " " << E << " " << EE << " " << r12 <<endl;
-		E_.append(E)
-		r12_.append(r12)
-		#cyles, beta E, E2 = data[:,0], data[:,1], data[:,2]
-		#print(cycles, beta, E, E2, r12)
-		
-		
-		axes[0].plot(alphas, E,label=mode) #,label=fr"$\alpha$ = {alpha:.1f}, E = {E[-1]:.2f}")
-		#axes[1].plot(cycles[i], 	varE)#, label=fr"$\alpha$ = {alpha:.1f} $\sigma_E^2$ = {varE[-1]:.2f}")
-		axes[1].plot(alphas, r12, label=mode)
-		#axes[1].plot(alphas, 1/r12, label=fr"$1/r_{12}000$")
-		#axes[1].legend()
-	#axes[1].plot(alphas, E_[0]-E_[1], label="E2-E1")
+			E, r12 = np.loadtxt(f"../data/alpha{mode}.dat", unpack=True, usecols=(3,5))
+			r12_.append(r12)
+			
+			axes[0].plot(alphas, E,label=labels[i],color=colors[i], lw=4) #,label=fr"$\alpha$ = {alpha:.1f}, E = {E[-1]:.2f}")
+			axes[1].plot(alphas, r12,label=labels[i],color=colors[i], lw=2+2.2-1.5*i,alpha=0.4*i/2+0.6, linestyle=linestyles[i]) #,label=fr"$\alpha$ = {alpha:.1f}, E = {E[-1]:.2f}")
 
-	#axes[1].plot(alphas, 1/r12, label=fr"$1/r_{12}1111$")
-	#EL2 = lambda b:
-	#b=0
-	#EL2 = E_[0] + 1/(2*(1+b*r12)**2) *(alphas*r12 -1/(2*(1+b*r12)**2) -2/r12+2*b/(1+b*r12))
-	#axes[1].plot(alphas, E_[0]- E_[1], label="E0-E1")
+		for i in range(2):
+			axes[i].set_xlabel(r"$\alpha$", fontsize=16)
+		axes[0].set_ylabel(r"$E_{L1}$ $[\omega]$", fontsize=16)
+		axes[1].set_ylabel(r"$r_{12}$", fontsize=18)
+		axes[0].set_title(r"Energy as function of $\alpha$", fontsize=17)
+		axes[1].set_title(r"Distance $r_{12}$ as function of $\alpha$", fontsize=17)
+		lines, labels = axes[0].get_legend_handles_labels()
+		fig.legend(lines, labels,loc='upper center', fancybox=True, ncol=2,shadow=True,fontsize=14)
+		#axes[0].legend()
+		plt.show()
 
-	#axes[1].plot(alphas, 1/r12,label=fr"1/r12")
 
-	#print(np.mean(EL2- E_[1] - 1/r12), np.mean(EL2))
-	axes[1].legend()
-	axes[0].legend()
-	plt.show()
-
-def varAlphaInteractive(sim = False):
-	import numpy as np
-	import matplotlib.pyplot as plt
-	import os
-
-	MCCs = 7
-	alphaStart = 0.85
-	alphaEnd = 1.15
-	dAlpha = 0.05
-
-	N_alpha = round(((alphaEnd-alphaStart)/dAlpha),0)
-	N_alpha = int(N_alpha) + 1
-
-	alphas = np.linspace(alphaStart, alphaEnd, N_alpha, endpoint=True)
-
-	if sim:
-		os.system(f"../compiled/variateAlpha.exe {7} {alphaStart} {alphaEnd} {dAlpha} interactive")
-
-	fig, axes = plt.subplots(nrows=1, ncols=2)
-	axes[0].set(xscale="log")
-	axes[1].set(xscale="log")
-	
-	data = np.loadtxt(f"../data/varAlphaInteractive.dat")
-	cyles, E, E2 = data[:,0], data[:,1], data[:,2]
-
-	varE = E2-E*E
-	skip = 0
-	#axes[0].plot(cyles[skip:], E[skip:], label=fr"$\alpha$ = {alpha:.2f}, E = {E[-1]:.2f}")
-	#axes[1].plot(cyles[skip:], varE[skip:], label=fr"$\alpha$ = {alpha:.2f} $\sigma_E^2$ = {varE[-1]:.2f}")
-
-	axes[0].legend()
-	axes[1].legend()
-	plt.show()
 
 varAlpha(sim = False)
