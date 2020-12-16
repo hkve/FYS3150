a8a99c69aa3886e5be7501c0d0403502d1dab1b5
hkve <haakon.kvernmoen@gmail.com>
2020-12-16 17:44:52 +0100

    Updated some indexing

--- Project5/code/plotting/plotVarBetaParallel.py
+++ Project5/code/plotting/plotVarBetaParallel.py
@@ -4,7 +4,7 @@ def main(sim = False):
 	import os,sys
 	from mpl_toolkits.mplot3d import Axes3D,art3d
 
-	idx, MCCs, alpha, beta, E, EE, r12 = np.loadtxt(f"../data/dump.dat", unpack=True)
+	idx, MCCs, alpha, beta, E, EE, r12 = np.loadtxt(f"../data/betaGridMCCs8_100.dat", unpack=True)
 
 	idx = np.argsort(idx)
 	
@@ -13,16 +13,17 @@ def main(sim = False):
 	beta = beta[idx]
 	alpha = np.unique(alpha)
 	beta = np.unique(beta)
-	
+	print(beta)
 	alpha, beta = np.meshgrid(alpha, beta)
-	
 	E = E.reshape(alpha.shape)
-	minIdx = np.unravel_index(np.argmin(E, axis=None), E.shape)
-	print(alpha[minIdx], beta[minIdx]) # This is alphaMin and betaMin
+	minIdxVar = np.unravel_index(np.argmin(E, axis=None), E.shape, order="F")
+	minIdxE = np.unravel_index(np.argmin(E, axis=None), E.shape, order="C")
+	print(alpha[minIdxVar], beta[minIdxVar], E[minIdxE]) # This is alphaMin and betaMin
+
 	
 	fig = plt.figure()	
 	ax = fig.gca(projection='3d')
-	ax.scatter(alpha[minIdx], beta[minIdx],E[minIdx], zorder=1000, s=20,label=f"Minima: $\\alpha = {round(alpha[minIdx],2)}, \\beta = {round(beta[minIdx],2)}, E = {round(E[minIdx],2)}$", color="black")
+	ax.scatter(alpha[minIdxVar], beta[minIdxVar],E[minIdxE], zorder=1000, s=20,label=rf"Minima: $\alpha = {round(alpha[minIdxVar],3)}, \beta = {round(beta[minIdxVar],3)}, \langle E \rangle = {round(E[minIdxE],4)}$", color="black")
 	surf = ax.plot_surface(alpha, beta, E,cmap="jet",linewidth=8, antialiased=True,alpha=0.95)#"turbo"
 
 	ax.set_xlabel(r"$\alpha$", fontsize=15)
@@ -31,3 +32,5 @@ def main(sim = False):
 	ax.set_title(r"Estimated ground state energy for varying $\alpha$ and $\beta$",fontsize=14)
 	ax.legend(loc='upper center', fancybox=True,shadow=True,fontsize=12)
 	plt.show()
+
+main()
